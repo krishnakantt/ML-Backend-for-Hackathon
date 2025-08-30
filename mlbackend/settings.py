767a3f5eb8dc5819f2ea8e4a250a55381a76ade1
Krishna Kant Das <kkant872002@yahoo.in>
2025-08-31 00:24:13 +0530

    Update settings.py

--- mlbackend/settings.py
+++ mlbackend/settings.py
@@ -58,7 +58,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = [
+    "https://test-frontend-tau-eight.vercel.app",
+]
 
 CORS_ALLOW_HEADERS = [
     "content-type",
